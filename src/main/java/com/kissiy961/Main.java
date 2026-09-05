dafebe8fb3b3c03cd3a1fddf521b6aa9ecabbeb3
kissiy961-dotcom <kissiy961@gmail.com>
2026-09-06 00:55:33 +0900

    メッセージを出したよ

--- src/main/java/com/kissiy961/Main.java
+++ src/main/java/com/kissiy961/Main.java
@@ -3,6 +3,8 @@ package com.kissiy961;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -10,9 +12,23 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
 
+        // 「こんにちは！」を表示する場所
+        Label message = new Label();
+
+        // ボタン
         Button button = new Button("クリック");
 
-        Scene scene = new Scene(button, 400, 300);
+        // ボタンを押したときの処理
+        button.setOnAction(event -> {
+            message.setText("こんにちは！");
+        });
+
+        // ボタンとメッセージを縦に並べる
+        VBox root = new VBox(20);
+        root.getChildren().addAll(button, message);
+
+        // ウィンドウ
+        Scene scene = new Scene(root, 400, 300);
 
         stage.setTitle("テスト");
         stage.setScene(scene);
